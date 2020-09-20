a524bcf984f7953394bc6027bf50c7100b2eaca1
Meserenko <meserenko.oleg@gmail.com>
2020-09-20 13:42:04 +0300

    homework

---
+++ script.js
@@ -0,0 +1,46 @@
+// 1.Write a JavaScript program which compute, the average marks of the following students Then, this average is used to determine the corresponding grade.
+// let arr = [80, 77, 68, 88, 95], sum = 0, result;
+// for (let i = 0; i<arr.length; i++) {
+//   sum+= arr[i];
+// }
+//
+// result = sum / arr.length;
+// if(result < 60) {
+//   console.log(`${result} - F`);
+// } else if (result < 70) {
+//   console.log(`${result} - D`);
+// } else if (result < 80) {
+//   console.log(`${result} - C`);
+// } else if (result < 90) {
+//   console.log(`${result} - B`);
+// } else if (result > 90 && result < 100) {
+//   console.log(`${result} - A`);
+// }
+
+
+
+
+// 2. Write a JavaScript program to sum the multiples of 3 or 5 under 1000.
+// let sum = 0;
+// const firstDivider = 3, secondDivider = 5
+// for (i=0; i < 1000; i++) {
+//   if (i % firstDivider === 0 || i % secondDivider === 0) {
+//     sum+= i;
+//   }
+// }
+// console.log(sum);
+
+
+
+
+// 3. Write a JavaScript program to construct the following pattern, using a nested for loop.
+// let symbol = '*', space = ' ';
+//
+// for (let i = 0; i < 1; i++) {
+//         symbol += space;
+//     for (let j = 0; j < 5; j++) {
+//         console.log(symbol);
+//         symbol += symbol[i] + space;
+//
+//     }
+// }
